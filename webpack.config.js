daad52b71c3345ad269dc163c1a261713a78d068
ipadorusa <ipadorusa@gmail.com>
2018-12-31 17:44:31 +0900

    'webpack_setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const cleanOptions = {
+  root:     '',
+  exclude:  [''],
+  verbose:  true,
+  dry:      false
+};
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/app.js',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css']
+  },
+  plugins: [
+    new CleanWebpackPlugin([
+        'dist'
+    ], cleanOptions)
+  ]
 };
